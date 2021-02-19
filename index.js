6f97676cf03be6dee0b5b5fffc77bad439dc4201
Prince-Mittal <princemittal555@gmail.com>
2021-02-19 22:35:41 +0530

    Removed Updater

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
-const { autoUpdater } = require('electron-updater');
+const { app, BrowserWindow, ipcMain } = require("electron");
 let mainWindow;
 
-function createWindow () {
+function createWindow() {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
@@ -10,38 +9,37 @@ function createWindow () {
       nodeIntegration: true,
     },
   });
-  mainWindow.loadFile('index.html');
-  mainWindow.on('closed', function () {
+  mainWindow.loadFile("index.html");
+  mainWindow.on("closed", function () {
     mainWindow = null;
   });
-    autoUpdater.checkForUpdatesAndNotify();
 }
 
-app.on('ready', () => {
+app.on("ready", () => {
   createWindow();
 });
 
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') {
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-app.on('activate', function () {
+app.on("activate", function () {
   if (mainWindow === null) {
     createWindow();
   }
 });
 
-ipcMain.on('app_version', (event) => {
-  event.sender.send('app_version', { version: app.getVersion() });
+ipcMain.on("app_version", (event) => {
+  event.sender.send("app_version", { version: app.getVersion() });
 });
-autoUpdater.on('update-available', () => {
-    mainWindow.webContents.send('update_available');
-  });
-  autoUpdater.on('update-downloaded', () => {
-    mainWindow.webContents.send('update_downloaded');
-  });
-  ipcMain.on('restart_app', () => {
-    autoUpdater.quitAndInstall();
-  });
+autoUpdater.on("update-available", () => {
+  mainWindow.webContents.send("update_available");
+});
+autoUpdater.on("update-downloaded", () => {
+  mainWindow.webContents.send("update_downloaded");
+});
+ipcMain.on("restart_app", () => {
+  autoUpdater.quitAndInstall();
+});
